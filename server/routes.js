31af90fa53da354735bdced6caa0800f52d65726
garofalof <fgarofalo1493@gmail.com>
2022-04-26 21:55:29 -0700

    added loader.io token

--- server/routes.js
+++ server/routes.js
@@ -9,5 +9,8 @@ router.get('/products', getProducts);
 router.get('/products/:productId', getProduct);
 router.get('/products/:productId/styles', getStyles);
 router.get('/products/:productId/related', getRelated);
+router.get('/loaderio-ceb9bc2865684926d4b5cdb52c81dbbb.txt', (req, res) => {
+  res.sendStatus(200);
+});
 
 module.exports = router;
