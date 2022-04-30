5888882ab6edffc4bddd88cc6da3e520edfd01ca
garofalof <fgarofalo1493@gmail.com>
2022-04-30 09:40:51 -0700

    added response time file

--- server/controllers.js
+++ server/controllers.js
@@ -1,4 +1,5 @@
 const os = require('os');
+const fs = require('fs');
 const models = require('./models');
 
 module.exports = {
@@ -9,7 +10,11 @@ module.exports = {
       const count = req.query.count || 5;
       const result = await models.getProducts(page, count);
       const t1 = performance.now() - t0;
-      console.log(`${os.hostname()} response time: ${t1}`);
+      fs.appendFile(`${__dirname}/response.txt`, `${os.hostname()} products res time: ${t1}ms \n`, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
       res.json(result.rows);
     } catch (err) {
       res.sendStatus(404);
@@ -18,7 +23,14 @@ module.exports = {
   getProduct: async (req, res) => {
     try {
       const { productId } = req.params;
+      const t0 = performance.now();
       const result = await models.getProduct(productId);
+      const t1 = performance.now() - t0;
+      fs.appendFile(`${__dirname}/response.txt`, `${os.hostname()} product res time: ${t1}ms \n`, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
       res.json(result.rows[0].product);
     } catch (err) {
       res.sendStatus(404);
@@ -27,7 +39,14 @@ module.exports = {
   getStyles: async (req, res) => {
     try {
       const { productId } = req.params;
+      const t0 = performance.now();
       const results = await models.getStyles(productId);
+      const t1 = performance.now() - t0;
+      fs.appendFile(`${__dirname}/response.txt`, `${os.hostname()} styles res time: ${t1}ms \n`, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
       const styles = results.rows[0].product;
       res.json(styles);
     } catch (err) {
@@ -37,7 +56,14 @@ module.exports = {
   getRelated: async (req, res) => {
     try {
       const { productId } = req.params;
+      const t0 = performance.now();
       const result = await models.getRelated(productId);
+      const t1 = performance.now() - t0;
+      fs.appendFile(`${__dirname}/response.txt`, `${os.hostname()} related res time: ${t1}ms \n`, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
       const { related } = result.rows[0];
       if (!related) {
         throw new Error();
