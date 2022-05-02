f0bcc5bd6f11ac3047fdf15cd0b330fabd82cd2e
garofalof <fgarofalo1493@gmail.com>
2022-05-02 10:26:44 -0700

    final commit

--- db/database.sql
+++ db/database.sql
@@ -49,26 +49,3 @@ CREATE TABLE related (
   relatedId INT NOT NULL,
   FOREIGN KEY (productId) REFERENCES products(id)
 );
-
---\c products to go into products database
---\dt to view tables
---\d table to describe specific table
---\copy products (id, name, slogan, description, category, defaultPrice) FROM './csv/product.csv' (FORMAT CSV, HEADER, DELIMITER ',');
---\copy tempFeatures FROM './csv/features.csv' (FORMAT CSV, HEADER, DELIMITER ',');
---\copy styles FROM './csv/styles.csv' (FORMAT CSV, null "null", HEADER, DELIMITER ',');
---\copy tempPhotos FROM './csv/photos.csv' (FORMAT CSV, HEADER, DELIMITER ',');
---\copy skus FROM './csv/skus.csv' (FORMAT CSV, HEADER, DELIMITER ',');
---\copy tempRelated FROM './csv/related.csv' (FORMAT CSV, HEADER, DELIMITER ',');
---ALTER TABLE features ADD CONSTRAINT productId FOREIGN KEY(productId) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE;
---ALTER TABLE features ALTER COLUMN productId SET NOT NULL;
---ALTER TABLE photos ALTER COLUMN styleId SET NOT NULL;
---ALTER TABLE photos ADD CONSTRAINT styleId FOREIGN KEY(styleId) REFERENCES styles(id) ON DELETE CASCADE ON UPDATE CASCADE;
---SELECT productId, relatedId INTO related FROM tempRelated;
---ALTER TABLE related ALTER COLUMN productId SET NOT NULL;
---ALTER TABLE related ALTER COLUMN relatedId SET NOT NULL;
---ALTER TABLE related ADD CONSTRAINT productId FOREIGN KEY(productId) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE;
---CREATE INDEX idx_features_productId on features(productId);
---CREATE INDEX idx_styles_productId on styles(productId);
---CREATE INDEX idx_photos_styleId on photos(styleId);
---CREATE INDEX idx_skus_styleId on skus(styleId);
---CREATE INDEX idx_related_productId on related(productId);
--- server/controllers.js
+++ server/controllers.js
@@ -1,20 +1,11 @@
-const os = require('os');
-const fs = require('fs');
 const models = require('./models');
 
 module.exports = {
   getProducts: async (req, res) => {
     try {
-      const t0 = performance.now();
       const page = req.query.page || 1;
       const count = req.query.count || 5;
       const result = await models.getProducts(page, count);
-      const t1 = performance.now() - t0;
-      fs.appendFile(`${__dirname}/response.txt`, `${os.hostname()} products res time: ${t1}ms \n`, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
       res.json(result.rows);
     } catch (err) {
       res.sendStatus(404);
@@ -23,14 +14,7 @@ module.exports = {
   getProduct: async (req, res) => {
     try {
       const { productId } = req.params;
-      const t0 = performance.now();
       const result = await models.getProduct(productId);
-      const t1 = performance.now() - t0;
-      fs.appendFile(`${__dirname}/response.txt`, `${os.hostname()} product res time: ${t1}ms \n`, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
       res.json(result.rows[0].product);
     } catch (err) {
       res.sendStatus(404);
@@ -39,14 +23,7 @@ module.exports = {
   getStyles: async (req, res) => {
     try {
       const { productId } = req.params;
-      const t0 = performance.now();
       const results = await models.getStyles(productId);
-      const t1 = performance.now() - t0;
-      fs.appendFile(`${__dirname}/response.txt`, `${os.hostname()} styles res time: ${t1}ms \n`, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
       const styles = results.rows[0].product;
       res.json(styles);
     } catch (err) {
@@ -56,14 +33,7 @@ module.exports = {
   getRelated: async (req, res) => {
     try {
       const { productId } = req.params;
-      const t0 = performance.now();
       const result = await models.getRelated(productId);
-      const t1 = performance.now() - t0;
-      fs.appendFile(`${__dirname}/response.txt`, `${os.hostname()} related res time: ${t1}ms \n`, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
       const { related } = result.rows[0];
       if (!related) {
         throw new Error();
